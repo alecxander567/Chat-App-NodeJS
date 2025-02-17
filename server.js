fbd51483424ba882cb614800dd9801da5ee359f9
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-02-17 17:59:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*", // Allow all origins
+    origin: "https://Chat-JS.netlify.app", // Allow only your frontend to connect
     methods: ["GET", "POST"]
   }
 });
