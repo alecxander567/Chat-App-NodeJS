9d0a0d436757036218495c5f847a6f1a03b9d932
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-02-21 01:41:01 +0800

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,33 +1,34 @@
 const socket = io("https://chat-app-nodejs-txw9.onrender.com");
-    let username = prompt("Enter your name:") || "Anonymous";
+let username = prompt("Enter your name:") || "Anonymous";
 
     
 document.getElementById('profile-name').innerText = username;
 
 
-    function sendMessage() {
-        let msg = document.getElementById('message').value.trim();
-        if (msg !== "") {
-            socket.emit('chatMessage', { user: username, text: msg });
-            document.getElementById('message').value = '';
-        }
+function sendMessage() {
+    let msg = document.getElementById('message').value.trim();
+    if (msg !== "") {
+        socket.emit('chatMessage', { user: username, text: msg });
+        document.getElementById('message').value = '';
     }
+}
 
 
 socket.on('chatMessage', (data) => {
-        let chatBox = document.getElementById('chat-box');
-        let messageClass = (data.user === username) ? 'sent' : 'received';
-        chatBox.innerHTML += `
-            <div class="message ${messageClass}">
-                <div class="text">
-                    <strong>${data.user}:</strong> ${data.text}
-                </div>
+    let chatBox = document.getElementById('chat-box');
+    let messageClass = (data.user === username) ? 'sent' : 'received';
+    chatBox.innerHTML += `
+        <div class="message ${messageClass}">
+            <div class="text">
+                <strong>${data.user}:</strong> ${data.text}
             </div>
-        `;
-        chatBox.scrollTop = chatBox.scrollHeight;
+        </div>
+    `;
+    chatBox.scrollTop = chatBox.scrollHeight;
 });
-   
 
-function toggleSidebar() {            document.getElementById('sidebar').classList.toggle('active');
+   
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('active');
 }
 
