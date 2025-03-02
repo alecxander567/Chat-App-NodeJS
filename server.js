b0400b82d40d4d8577f43dbd8ada5d5bee24862a
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-03-02 14:05:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -12,30 +11,43 @@ const io = socketIo(server, {
   }
 });
 
+let users = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
+socket.on("register", (username) => {
+  users[username] = socket.id;
+  io.emit("userList", Object.keys(users)); 
+});
 
 socket.on("chatMessage", (data) => {
     io.emit("chatMessage", data); 
   });
 
-
-socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+socket.on("privateMessage", ({ sender, receiver, text }) => {
+  const receiverSocketId = users[receiver]; 
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("privateMessage", { sender, text });
+    }
 });
 
-
+socket.on("disconnect", () => {
+    let userToRemove = Object.keys(users).find((key) => users[key] === socket.id);
+    if (userToRemove) {
+      delete users[userToRemove];
+      io.emit("userList", Object.keys(users)); 
+    }
+    console.log(`User ${socket.id} disconnected`);
+  });
+}); 
+
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
   res.send("Chat App Server is Running!");
 });
 
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
