b34c195b3bf7a29f124dca1376c0a385629f5c30
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-03-03 08:58:29 +0800

    Add files via upload

---
+++ showChat.js
@@ -0,0 +1,11 @@
+document.getElementById("private").style.display = "none";
+
+function showPrivate() {
+    document.getElementById("private").style.display = "block";
+    document.getElementById("public").style.display = "none";
+}
+
+function showPublic() {
+    document.getElementById("public").style.display = "block";
+    document.getElementById("private").style.display = "none";
+}
